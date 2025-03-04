4a5fdc042b6884f6fafa8de3249b01e3e12d04b1
LeandroSWR <leandro-xy@hotmail.com>
2025-03-04 19:49:20 +0000

    Start up the next learning project an investment calculator.

---
+++ 03-investment-calculator/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/learningReact/investment-calculator", // Set the base URL
+  plugins: [react()],
+  build: {
+    outDir: "../docs/investment-calculator" // Change build output directory
+  }
+});
+

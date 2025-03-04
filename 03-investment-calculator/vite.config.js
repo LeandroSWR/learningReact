a7820b7b159cf544208111302daaad2abc249a9e
LeandroSWR <leandro-xy@hotmail.com>
2025-03-04 20:36:04 +0000

    Test

--- 03-investment-calculator/vite.config.js
+++ 03-investment-calculator/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/learningReact/investment-calculator", // Set the base URL
+  base: "/investment-calculator", // Set the base URL
   plugins: [react()],
   build: {
     outDir: "../docs/projects/investment-calculator" // Change build output directory

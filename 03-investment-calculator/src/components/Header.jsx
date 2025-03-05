f1a7e2fe9117952444ae8ad78e565d49ccbc2258
LeandroSWR <leandro-xy@hotmail.com>
2025-03-05 02:39:41 +0000

    Create the Header component and unlock the css for it.

---
+++ 03-investment-calculator/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from '../../public/investment-calculator-logo.png'
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} alt="logo" />
+      <h1>React Investment Calculator</h1>
+    </header>
+  )
+}
